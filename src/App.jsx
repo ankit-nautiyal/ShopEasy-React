7a455c65014847dab9a719f3fe21bae8917f1589
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-04-13 00:57:24 +0530

    add router in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
+import {Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -9,18 +8,18 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
+    <>
         <Header />
+
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
+
         <Footer />
-      </Router>
-    </CartProvider>
+    </>
   );
 }
 
